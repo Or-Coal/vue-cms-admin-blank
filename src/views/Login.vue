c604aa7d5ad9d61b0e79af9e1c90afa1f8fcbfe4
Or-Coal <975015584@qq.com>
2022-09-09 14:29:40 +0800

    按需引入element-plus,删除修改vue初始化文件

---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+    <div> 
+        <el-button type="success">Success</el-button>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang='less' scoped> 
+</style>
